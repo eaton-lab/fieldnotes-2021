8acaa8412642a773dfbf3545f5ab3bb3ace6fec6
Deren <de2356@columbia.edu>
2021-07-01 20:53:00 -0400

    update

--- docs/convert-photos.py
+++ docs/convert-photos.py
@@ -11,6 +11,7 @@ import glob
 if __name__ == "__main__":
 
     photos = glob.glob("/home/deren/Desktop/collection-pics/*.jpg")
+    photos += glob.glob("/home/deren/Desktop/collection-pics/*.png")
     for photo in photos:
         name = os.path.basename(photo)
         newpath = os.path.join("./assets/flowers", name)
